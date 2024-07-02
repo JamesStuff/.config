961068c0e3c797386d9513271dc9f92ab967181b
James Robinson <james@jamesrob.com>
2024-07-02 14:20:50 +0100

    cmp colour tests

--- nvim/lua/jimbo/core/colors.lua
+++ nvim/lua/jimbo/core/colors.lua
@@ -2,3 +2,8 @@
 
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" } )
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } )
+
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "#242933" }) -- Replace "#1e222a" with your desired background color
+
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1b1f26" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#1b1f26", fg = "#3e4452" })
--- nvim/lua/jimbo/plugins/lsp/lspconfig.lua
+++ nvim/lua/jimbo/plugins/lsp/lspconfig.lua
@@ -24,6 +24,14 @@ return {
         -- Adds black background to auto completion box
         vim.api.nvim_set_hl(0, "CmpNormal", { bg = "#000000" })
 
+        -- -- Random chat gippity stuff
+        -- -- -- Set up custom highlight groups for completion items
+        -- vim.api.nvim_set_hl(0, "CmpItemAbbr", { fg = "#FFFFFF" })
+        -- vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#569CD6", bold = true })
+        -- vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#569CD6", underline = true })
+        -- vim.api.nvim_set_hl(0, "CmpItemKind", { fg = "#C586C0" })
+        -- vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#9CDCFE", italic = true })
+
         -- Executes logic when an LSP attaches to a buffer
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
