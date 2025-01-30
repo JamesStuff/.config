e3c0601692ca2b26ef80257d570be1d14bffca75
James Robinson <james@jamesrob.com>
2025-01-30 16:30:27 +0000

    Added easy header swtiching for c/cpp

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -2,3 +2,5 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.expandtab = true
+
+vim.keymap.set("n", "gh", "<cmd>ClangdSwitchSourceHeader<cr>", { desc = "Switch Source/Header (C/C++)" })
